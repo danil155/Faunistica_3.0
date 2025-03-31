22d2f41c33f873d612ef311a9bef4941fe0b0e38
danii <daniilparschin186@gmail.com>
2025-03-31 23:21:39 +0500

    correct get_taxon_info(), find_species_in_text(), correct code style

--- text_processing/gbif_parser.py
+++ text_processing/gbif_parser.py
@@ -1,36 +1,21 @@
 from pygbif import species as gbif_species
 import re
 
-def extract_latin_words(text):
-    #Извлекает слова на латинице из текста
-    return re.findall(r'\b[A-Za-z]+\b', text)
 
-def find_species_in_text(text):
-    # поиск названия видов среди подряд идущих слов на латинице
-    latin_words = extract_latin_words(text)
-    
-    for i in range(len(latin_words) - 1):
-        candidate = f"{latin_words[i]} {latin_words[i+1]}"
-        is_valid, data = validate_species_name(candidate)
-        
-        if is_valid and not data.get('error'):
-            return candidate, data
-    
-    return None, None
-
-def get_taxon_info(scientific_name):
-    # Поиск названия в GBIF
+# Поиск названия в GBIF
+def get_taxon_info(scientific_name: str) -> gbif_species:
     try:
         return gbif_species.name_backbone(
-            name=scientific_name, 
-            rank='species', 
+            name=scientific_name,
+            rank='species',
             strict=True
         )
-    except Exception:
-        return None
+    except Exception as e:
+        raise Exception(scientific_name) from e
 
-def validate_species_name(scientific_name):
-    # Проверяет название вида и возвращает таксономические данные
+
+# Проверяет название вида и возвращает таксономические данные
+def validate_species_name(scientific_name: str) -> [bool, dict]:
     parts = scientific_name.strip().split()
     if len(parts) != 2:
         return False, {"error": "Некорректный формат названия"}
@@ -49,11 +34,28 @@ def validate_species_name(scientific_name):
     }
 
 
+# Поиск названия видов среди подряд идущих слов на латинице
+def find_species_in_text(text: str) -> [str, dict]:
+    latin_words = re.findall(r'\b[A-Za-z]+\b', text)
+
+    for i in range(len(latin_words) - 1):
+        candidate = f"{latin_words[i]} {latin_words[i + 1]}"
+        is_valid, data = validate_species_name(candidate)
+
+        if is_valid and not data.get('error'):
+            return candidate, data
+
+    return None, None
+
+
 if __name__ == "__main__":
+    get_taxon_info("sdfsdfsdf")
     while True:
         text = input("Введите текст для анализа (пример: 'Runcinia tarabayevi Marusik et Logunov, 1990 Материал. 1 ♂, Челябинская обл., Троицкий р-н, заказник Троицкий, березовый колок, укосы, 29.VI.2023, Устинова А.Л.'): ")
         found_name, data = find_species_in_text(text)
-        
+        print(found_name)
+        print(data)
+
         if not found_name:
             print("Валидные названия не обнаружены")
             continue
